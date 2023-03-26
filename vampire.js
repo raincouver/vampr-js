cc00886af0e09271c60115868f24c508e79a9193
Rain Zhu <rain1995zhu@gmail.com>
2023-03-25 22:50:39 -0700

    added vampr app

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentCreator = this.creator;
+    while (currentCreator) {
+      currentCreator = currentCreator.creator;
+      numberOfVampires ++;
+    }
 
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -37,6 +45,36 @@ class Vampire {
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
 
+    //return original if this is the original
+    if (!this.creator) {
+      return this;
+    }
+
+    //find the sibling of the lower lineage vampire in comparison
+    let myCreator = this;
+    let lineageDifference = Math.abs(this.numberOfVampiresFromOriginal - vampire.numberOfVampiresFromOriginal);
+
+    if (this.isMoreSeniorThan(vampire)) {
+      while (lineageDifference > 0) {
+        vampire = vampire.creator;
+        lineageDifference --;
+      }
+    }
+
+    if (!this.isMoreSeniorThan(vampire)) {
+      while (lineageDifference > 0) {
+        myCreator = myCreator.creator;
+        lineageDifference --;
+      }
+    }
+
+    //find the common ancestor of the sibling and the higher lineage vampire in comparison
+    while (myCreator !== vampire) {
+      myCreator = myCreator.creator;
+      vampire = vampire.creator;
+    }
+
+    return vampire;
   }
 }
 
